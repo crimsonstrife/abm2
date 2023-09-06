4b457f9385dfddab06439c1f994fb0efe2c060f5
Patrick Barnhardt <pbarnhardt@charlottepipe.com>
2023-09-06 15:34:33 -0400

    Update MainActivity.java

--- app/src/main/java/com/pbarnhardt/abm2task1/MainActivity.java
+++ app/src/main/java/com/pbarnhardt/abm2task1/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * The type Main activity.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
