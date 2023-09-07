d00df687deecb03d951346da8351592f3e29a1fc
Patrick Barnhardt <pbarnhardt@charlottepipe.com>
2023-09-07 15:28:52 -0400

    Update MainActivity.java

--- app/src/main/java/com/pbarnhardt/abm2task1/MainActivity.java
+++ app/src/main/java/com/pbarnhardt/abm2task1/MainActivity.java
@@ -1,13 +1,34 @@
 package com.pbarnhardt.abm2task1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * The type Main activity.
  */
 public class MainActivity extends AppCompatActivity {
+    /**
+     * Variables
+     */
+    Toast toast;
+    String date = null;
+    String time = null;
+
+    /**
+     * The constant DATE_FORMAT.
+     */
+    public static final String DATE_FORMAT = "MM/dd/yyyy";
+    /**
+     * The constant TIME_FORMAT.
+     */
+    public static final String TIME_FORMAT = "hh:mm a";
+
+    /**
+     * The constant timePos
+     */
+    public static int timePos = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
