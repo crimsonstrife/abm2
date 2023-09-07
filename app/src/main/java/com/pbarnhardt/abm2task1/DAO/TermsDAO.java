11895874896fb2b0d815be9b92dd629fccb65fc3
Patrick Barnhardt <pbarnhardt@charlottepipe.com>
2023-09-07 15:28:22 -0400

    Create TermsDAO.java

---
+++ app/src/main/java/com/pbarnhardt/abm2task1/DAO/TermsDAO.java
@@ -0,0 +1,57 @@
+package com.pbarnhardt.abm2task1.DAO;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.pbarnhardt.abm2task1.Entity.Terms;
+
+import java.util.List;
+
+/**
+ * The interface Terms dao.
+ */
+@Dao
+public interface TermsDAO {
+    /**
+     * Gets terms.
+     *
+     * @return the terms
+     */
+    // @Query("SELECT * FROM Terms ORDER BY termId")
+    @Query("SELECT * FROM Terms")
+    public List<Terms> getTerms();
+
+    /**
+     * Insert terms.
+     *
+     * @param term the term
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    public void insertTerms(Terms term);
+
+    /**
+     * Delete terms.
+     *
+     * @param term the term
+     */
+    @Delete
+    public void deleteTerms(Terms term);
+
+    /**
+     * Update terms.
+     *
+     * @param term the term
+     */
+    @Update
+    public void updateTerms(Terms term);
+
+    /**
+     * Delete all terms.
+     */
+    @Query("DELETE FROM Terms")
+    public void deleteAllTerms();
+}
