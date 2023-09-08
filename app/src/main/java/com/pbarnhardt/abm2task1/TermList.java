50af4d1e22b72cd70da4fac1028abbeb38e95344
Patrick Barnhardt <pbarnhardt@charlottepipe.com>
2023-09-08 14:48:53 -0400

    Create TermList.java

---
+++ app/src/main/java/com/pbarnhardt/abm2task1/TermList.java
@@ -0,0 +1,14 @@
+package com.pbarnhardt.abm2task1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TermList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_list);
+    }
+}
