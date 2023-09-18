ad9d89258b421b7edcd42a15368f42ae9cd5621e
Patrick Barnhardt <pbarnhardt@charlottepipe.com>
2023-09-18 16:01:38 -0400

    Create TermDetailsActivity.java

---
+++ app/src/main/java/com/pbarnhardt/abm2task1/Views/TermDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.pbarnhardt.abm2task1.Views;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.pbarnhardt.abm2task1.R;
+
+public class TermDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_details);
+    }
+}
