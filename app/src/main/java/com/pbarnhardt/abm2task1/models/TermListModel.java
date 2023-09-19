d76200e0fa6c913774ff0e6b76047a642414dec5
Patrick Barnhardt <pbarnhardt@charlottepipe.com>
2023-09-19 15:27:35 -0400

    Update TermListModel.java

--- app/src/main/java/com/pbarnhardt/abm2task1/models/TermListModel.java
+++ app/src/main/java/com/pbarnhardt/abm2task1/models/TermListModel.java
@@ -1,15 +1,34 @@
 package com.pbarnhardt.abm2task1.Models;
 
+import android.view.LayoutInflater;
+
 import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.pbarnhardt.abm2task1.Database.CourseRepository;
 import com.pbarnhardt.abm2task1.Entity.Terms;
+import com.pbarnhardt.abm2task1.R;
 
 import java.util.List;
 
 public class TermListModel extends ViewModel {
 
     private CourseRepository courseRepository;
+    private List<Terms> listOfAllTerms;
+    private LayoutInflater inflater;
+    private RecyclerView recycler;
+
+    private ViewModelProvider.Factory viewModelFactory;
+    private TermListModel termListModel;
+
+    int getContentViewId() {
+        return R.layout.activity_term_list;
+    }
+
+    int getNavigationMenuItemId() {
+        return R.id.termsNavAction;
+    }
 
     public TermListModel(CourseRepository courseRepository) {
         this.courseRepository = courseRepository;
