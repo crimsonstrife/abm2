a2f50d1d8062c92678d5c242ebe6aabc4d194c3a
Patrick Barnhardt <pbarnhardt@charlottepipe.com>
2023-09-20 09:01:30 -0400

    Update MentorsDAO.java

--- app/src/main/java/com/pbarnhardt/abm2task1/DAO/MentorsDAO.java
+++ app/src/main/java/com/pbarnhardt/abm2task1/DAO/MentorsDAO.java
@@ -1,7 +1,11 @@
 package com.pbarnhardt.abm2task1.DAO;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import com.pbarnhardt.abm2task1.Entity.Mentors;
 
@@ -21,4 +25,42 @@ public interface MentorsDAO {
     @Query("SELECT * FROM CourseMentors")
     public List<Mentors> getMentors();
 
+    /**
+     * Insert mentors.
+     *
+     * @param mentor the mentor
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    public void insertMentors(Mentors mentor);
+
+    /**
+     * Delete mentors.
+     *
+     * @param mentor the mentor
+     */
+    @Delete
+    public void deleteMentors(Mentors mentor);
+
+    /**
+     * Update mentors.
+     *
+     * @param mentor the mentor
+     */
+    @Update
+    public void updateMentors(Mentors mentor);
+
+    /**
+     * Delete all mentors.
+     */
+    @Query("DELETE FROM CourseMentors")
+    public void deleteAllMentors();
+
+    /**
+     * Gets mentors by course id.
+     *
+     * @param courseId the course id
+     * @return the mentors by course id
+     */
+    @Query("SELECT * FROM CourseMentors WHERE courseId = :courseId")
+    public List<Mentors> getMentorsByCourseId(int courseId);
 }
