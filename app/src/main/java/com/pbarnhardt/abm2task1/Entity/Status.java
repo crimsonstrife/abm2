f8cf5a3627a21467e45ed7b8c80c682884115656
Patrick Barnhardt <pbarnhardt@charlottepipe.com>
2023-09-20 16:32:11 -0400

    Create Status.java

---
+++ app/src/main/java/com/pbarnhardt/abm2task1/Entity/Status.java
@@ -0,0 +1,57 @@
+package com.pbarnhardt.abm2task1.Entity;
+
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+/**
+ * The type Statuses for courses.
+ */
+@Entity(tableName = "CourseStatus")
+public class Status {
+    @PrimaryKey(autoGenerate = true)
+    private int statusId;
+
+    private String statusText;
+
+    /**
+     * Instantiates a new Status.
+     *
+     * @param statusText the status text
+     */
+    public Status(String statusText) {
+        this.statusText = statusText;
+    }
+
+    /**
+     * Gets status id.
+     *
+     * @return the status id
+     */
+    public int getStatusId() {
+        return statusId;
+    }
+
+    /**
+     * Gets status text.
+     *
+     * @return the status text
+     */
+    public String getStatusText() {
+        return statusText;
+    }
+
+    /**
+     * Sets status text.
+     *
+     * @param statusText the status text
+     */
+    public void setStatusText(String statusText) {
+        this.statusText = statusText;
+    }
+
+    @Override
+    public String toString() {
+        return statusText;
+    }
+}
