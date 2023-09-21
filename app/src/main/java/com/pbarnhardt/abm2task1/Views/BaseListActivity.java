cb8d5cf0a56542fdf36d5a6bb1f0e29087f65f46
Patrick Barnhardt <pbarnhardt@charlottepipe.com>
2023-09-21 11:30:16 -0400

    Update BaseListActivity.java

--- app/src/main/java/com/pbarnhardt/abm2task1/Views/BaseListActivity.java
+++ app/src/main/java/com/pbarnhardt/abm2task1/Views/BaseListActivity.java
@@ -43,21 +43,19 @@ public abstract class BaseListActivity extends AppCompatActivity implements Navi
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         navigationView.postDelayed(() -> {
             int itemId = item.getItemId();
-            switch (itemId) {
-                case R.id.homeNavAction:
-                    startActivity(new Intent(this, MainActivity.class));
-                    break;
-                case R.id.termsNavAction:
-                    startActivity(new Intent(this, TermsListActivity.class));
-                    break;
-                case R.id.coursesNavAction:
-                    startActivity(new Intent(this, CoursesListActivity.class));
-                    break;
-                case R.id.assessmentsNavAction:
-                    startActivity(new Intent(this, AssessmentsListActivity.class));
-                    break;
+            //originally planned to use a switch statement here, but this caused an IDE error regarding the use of Resource IDs and them being non-final
+            //might've been more performant, but in this particular case I don't think it matters, and I avoid the error/warnings.
+            if(itemId == R.id.homeNavAction){
+                startActivity(new Intent(this, MainActivity.class));
+            } else if(itemId == R.id.termsNavAction){
+                startActivity(new Intent(this, TermsListActivity.class));
+            } else if(itemId == R.id.coursesNavAction){
+                startActivity(new Intent(this, CoursesListActivity.class));
+            } else if(itemId == R.id.assessmentsNavAction){
+                startActivity(new Intent(this, AssessmentsListActivity.class));
+            } else {
+                finish();
             }
-            finish();
         }, 300);
         return true;
     }
@@ -79,6 +77,19 @@ public abstract class BaseListActivity extends AppCompatActivity implements Navi
     abstract void populateScreen();
 
     public ItemTouchHelper.Callback createHelperCallback() {
-        return null;
+        ItemTouchHelper.SimpleCallback simpleTouchCallback = new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            //swipe helper
+            @Override
+            public void onSwiped(@NonNull androidx.recyclerview.widget.RecyclerView.ViewHolder viewHolder, int direction) {
+                //TODO: implement swipe features
+            }
+
+            //drag helper
+            @Override
+            public boolean onMove(@NonNull androidx.recyclerview.widget.RecyclerView recyclerView, @NonNull androidx.recyclerview.widget.RecyclerView.ViewHolder viewHolder, @NonNull androidx.recyclerview.widget.RecyclerView.ViewHolder target) {
+                return false; //return false so that the drag feature is disabled
+            }
+        };
+        return simpleTouchCallback;
     }
 }
