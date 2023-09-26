8b5828df7450e5cb0488129d983f256307f718e5
Patrick Barnhardt <pbarnhardt@charlottepipe.com>
2023-09-26 15:50:06 -0400

    Create CourseAdapter.java

---
+++ app/src/main/java/com/pbarnhardt/abm2task1/Adapters/CourseAdapter.java
@@ -0,0 +1,100 @@
+package com.pbarnhardt.abm2task1.Adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.pbarnhardt.abm2task1.Entity.Courses;
+import com.pbarnhardt.abm2task1.Enums.RecyclerAdapter;
+import com.pbarnhardt.abm2task1.R;
+import com.pbarnhardt.abm2task1.Utils.Formatting;
+import com.pbarnhardt.abm2task1.Views.CourseDetailsActivity;
+
+import java.util.List;
+
+public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.ViewHolder>{
+    private final List<Courses> listedCourses;
+    private final Context theContext;
+    private final RecyclerAdapter recyclerContext;
+    private CourseSelection courseSelected;
+
+    public interface CourseSelection {
+        void onCourseSelected(int position, Courses course);
+    }
+
+    public CourseAdapter(List<Courses> listedCourses, Context theContext, RecyclerAdapter recyclerContext, CourseSelection courseSelected) {
+        this.listedCourses = listedCourses;
+        this.theContext = theContext;
+        this.recyclerContext = recyclerContext;
+        this.courseSelected = courseSelected;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        //bind views here
+        TextView courseTitleView = itemView.findViewById(R.id.course_title);
+        TextView courseDatesView = itemView.findViewById(R.id.course_dates);
+        //bind button
+        FloatingActionButton courseFloatingActionButton = itemView.findViewById(R.id.course_floatingEditButton);
+        ImageButton courseDetailsButton = itemView.findViewById(R.id.courseDetails);
+        CourseSelection courseSelected;
+        public ViewHolder(@NonNull View itemView, CourseSelection courseSelected) {
+            super(itemView);
+            this.courseSelected = courseSelected;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            courseSelected.onCourseSelected(getAdapterPosition(), listedCourses.get(getAdapterPosition()));
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.card_list_courses, parent, false);
+        return new ViewHolder(view, courseSelected);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final Courses course = listedCourses.get(position);
+        holder.courseTitleView.setText(course.getCourseName());
+        String startAndEnd = Formatting.dateFormat.format(course.getCourseStartDate()) + " - " + Formatting.dateFormat.format(course.getCourseEndDate());
+        holder.courseDatesView.setText(startAndEnd);
+
+        switch (recyclerContext) {
+            case MAIN:
+                holder.courseFloatingActionButton.setImageDrawable(ContextCompat.getDrawable(theContext, R.drawable.ic_action_edit));
+                holder.courseDetailsButton.setOnClickListener(v -> {
+                    Intent intent = new Intent(theContext, CourseDetailsActivity.class);
+                    intent.putExtra("COURSE_ID", course.getCourseId());
+                    theContext.startActivity(intent);
+                });
+                break;
+            case CHILD:
+                holder.courseFloatingActionButton.setImageDrawable(ContextCompat.getDrawable(theContext, R.drawable.ic_action_delete));
+                holder.courseDetailsButton.setOnClickListener(v -> {
+                    if (courseSelected != null) {
+                        courseSelected.onCourseSelected(position, course);
+                    }
+                });
+                break;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return listedCourses.size();
+    }
+}
