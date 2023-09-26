e812354d600e47a32e8ef85091a33e121f70b4ca
Patrick Barnhardt <pbarnhardt@charlottepipe.com>
2023-09-26 15:49:22 -0400

    Create Constants.java

---
+++ app/src/main/java/com/pbarnhardt/abm2task1/Utils/Constants.java
@@ -0,0 +1,9 @@
+package com.pbarnhardt.abm2task1.Utils;
+
+public class Constants {
+    public static final String TERM_KEY = "term_key";
+    public static final String EDIT_KEY = "edit_key";
+    public static final String COURSE_KEY = "course_id_key";
+    public static final String ASSESSMENT_KEY = "assessment_key";
+    public static final String MENTOR_KEY = "mentor_key";
+}
