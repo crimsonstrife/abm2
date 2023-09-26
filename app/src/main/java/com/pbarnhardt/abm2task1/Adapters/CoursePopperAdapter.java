51358412f2fe2ee545e3a7a1a75a4a5770f34d48
Patrick Barnhardt <pbarnhardt@charlottepipe.com>
2023-09-26 15:50:03 -0400

    Create CoursePopperAdapter.java

---
+++ app/src/main/java/com/pbarnhardt/abm2task1/Adapters/CoursePopperAdapter.java
@@ -0,0 +1,75 @@
+package com.pbarnhardt.abm2task1.Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.pbarnhardt.abm2task1.Entity.Courses;
+import com.pbarnhardt.abm2task1.R;
+
+import java.util.List;
+
+public class CoursePopperAdapter  extends RecyclerView.Adapter<CoursePopperAdapter.ViewHolder> {
+    private List<Courses> coursesList;
+    private CourseListener courseListener;
+
+    /**
+     * Constructor
+     * @param coursesList
+     */
+    public CoursePopperAdapter(List<Courses> coursesList) {
+        super();
+        this.coursesList = coursesList;
+    }
+
+    /**
+     * Listener
+     * @param courseListener
+     */
+    public void setCourseListener(CoursePopperAdapter.CourseListener courseListener) {
+        this.courseListener = courseListener;
+    }
+
+    @NonNull
+    @Override
+    public CoursePopperAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_course, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CoursePopperAdapter.ViewHolder holder, int position) {
+        final Courses course = coursesList.get(position);
+        holder.itemTitle.setText(course.getCourseName());
+        holder.courseItemAddIcon.setOnClickListener(view -> {
+            if(courseListener != null) {
+                courseListener.onCourseSelected(position, course);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return coursesList.size();
+    }
+
+    public interface CourseListener {
+        void onCourseSelected(int position, Courses course);
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView itemTitle;
+        ImageView courseItemAddIcon;
+
+        public ViewHolder(View item) {
+            super(item);
+            itemTitle = item.findViewById(R.id.itemTitle);
+            courseItemAddIcon = item.findViewById(R.id.courseItemAddIcon);
+        }
+
+    }
+}
