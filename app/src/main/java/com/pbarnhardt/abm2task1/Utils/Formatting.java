fcc089ba73a75698746e01f77505260e8a11c641
Patrick Barnhardt <pbarnhardt@charlottepipe.com>
2023-09-26 15:49:19 -0400

    Update Formatting.java

--- app/src/main/java/com/pbarnhardt/abm2task1/Utils/Formatting.java
+++ app/src/main/java/com/pbarnhardt/abm2task1/Utils/Formatting.java
@@ -5,4 +5,5 @@ import java.text.SimpleDateFormat;
 public class Formatting {
     private static String pattern = "MM/dd/yyyy";
     public static SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+    public static String email = "^[A-Za-z0-9+_.-]+@(.+)$";
 }
