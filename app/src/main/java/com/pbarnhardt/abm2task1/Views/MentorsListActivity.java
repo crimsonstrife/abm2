3b4ac07d74ccb45daf8b97853e9b1b11284befa5
Patrick Barnhardt <pbarnhardt@charlottepipe.com>
2023-09-27 15:33:11 -0400

    Update MentorsListActivity.java

--- app/src/main/java/com/pbarnhardt/abm2task1/Views/MentorsListActivity.java
+++ app/src/main/java/com/pbarnhardt/abm2task1/Views/MentorsListActivity.java
@@ -41,7 +41,7 @@ public class MentorsListActivity extends AppCompatActivity implements MentorAdap
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mentor_list)
+        setContentView(R.layout.activity_mentor_list);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         initializeRecyclerView();
